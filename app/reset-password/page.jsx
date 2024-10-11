68501cbda18da86901f36521564b0b59c89ca445
Ridwan <74691968+Fasasi-tech@users.noreply.github.com>
2024-10-11 10:29:53 +0100

    added suspense to reset-password

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ResetPassword from '../ui/resetPassword/ResetPassword'
-
+import {Suspense} from "react";
 const page = () => {
   return (
     <div className="mx-4 mt-4">
+      <Suspense>
         <ResetPassword />
+      </Suspense>
     </div>
   )
 }
