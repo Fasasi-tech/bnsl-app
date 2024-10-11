8dfaa972196878a5c5eaa270bce2c3c0fa42d0ad
Ridwan <74691968+Fasasi-tech@users.noreply.github.com>
2024-10-11 10:57:51 +0100

    socket.io

--- app/ui/utils/socket.js
+++ app/ui/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000"); // Use your backend URL
+const socket = io("https://vendor-application.onrender.com"); // Use your backend URL
 
 export default socket;
